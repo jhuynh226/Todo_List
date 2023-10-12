9829568f7eacd7e984f3dee12a914d8c467fa733
jhuynh226 <137665975+jhuynh226@users.noreply.github.com>
2023-10-12 06:34:03 -0400

    Update 11-todo-list.js

--- JavaScript/11-todo-list.js
+++ JavaScript/11-todo-list.js
@@ -26,6 +26,7 @@ let todo_list_two = [];
 const add_button_two = document.querySelector('.add_button_two');
 const todo_input_two = document.querySelector('.todo_input_two');
 
+//Load local storage and retrieve todo list
 document.addEventListener("DOMContentLoaded", function () {
   const storedData = localStorage.getItem("todo_list_two");
   if (storedData) {
@@ -44,8 +45,12 @@ todo_input_two.onkeydown = function() {
   }
 }
 
+//add todo item todo list and display todo list at the end
 function add_todo_two(todo_list_two) {
+  //create empty string to change into innerHTML div later
   let todo_HTML = '';
+  
+  //create variables for input and date and store it into object
   const todo_input = document.querySelector('.todo_input_two');
   const todo_date = document.querySelector('.todo_date_two');
   const todo_object = { };
@@ -55,17 +60,18 @@ function add_todo_two(todo_list_two) {
   console.log(todo_object);
 
 
+  //store object into array and check if there is anything inside the input field
   if (todo_input.value != "") {
     todo_list_two.push(todo_object);
-    console.log(todo_list_two);
   }
 
   else {
 
   }
 
+  //loop to add everything inside the todolist array into the empty html
+  //string made earlier
   for(let i = 0; i < todo_list_two.length; i++) {
-    console.log(todo_list_two);
     const HTML = `
       <div>${todo_list_two[i].name}</div>
       <div>${todo_list_two[i].date}</div>
@@ -74,29 +80,25 @@ function add_todo_two(todo_list_two) {
         show_todo_two(todo_list_two);
       ">Delete</button>
     `;
-
-
     todo_HTML = todo_HTML + HTML;
-    console.log(todo_HTML);
   }
 
+
+  //add the completed html string into the todo list div
   let todo_list_div = document.querySelector('.todo_list_div');
   todo_list_div.innerHTML = todo_HTML;
 
+  //clear input field and add new array to local storage
   todo_input.value = '';
   localStorage.setItem("todo_list_two", JSON.stringify(todo_list_two));
 }
 
+//display todo list
 function show_todo_two (todo_list_two) {
+  //create empty string to change into innerHTML div later
   let todo_HTML = '';
-  const todo_input = document.querySelector('.todo_input_two');
-  const todo_date = document.querySelector('.todo_date_two');
-  const todo_object = { };
-
-  todo_object.name = todo_input.value;
-  todo_object.date = todo_date.value;
-  console.log(todo_object);
-
+  
+  //loop to display everything inside toolist array by storing values into empty string made earlier
   for(let i = 0; i < todo_list_two.length; i++) {
     console.log(todo_list_two);
     const HTML = `
@@ -107,15 +109,15 @@ function show_todo_two (todo_list_two) {
         show_todo_two(todo_list_two);
       ">Delete</button>
     `;
-
-
     todo_HTML = todo_HTML + HTML;
-    console.log(todo_HTML);
   }
 
+  //add the completed html string into the todo list div
   let todo_list_div = document.querySelector('.todo_list_div');
   todo_list_div.innerHTML = todo_HTML;
 
+  //clear input field and add new array to local storage
+  const todo_input = document.querySelector('.todo_input_two');
   todo_input.value = '';
   localStorage.setItem("todo_list_two", JSON.stringify(todo_list_two));
 }
